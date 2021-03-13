a33b1c7ca11a50ebc696883ce980708584ecb53d
Alexandr Grinchak <grinchak.alexandr@gmail.com>
2021-03-13 13:33:06 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0) {
+        n = n * -1;
+    }
+  let u = String(n);
+  let a = u.split("");
+  let x = a.reverse();
+  let y = x.join("");
+  let t = Number(y);
+  return t;
 }
